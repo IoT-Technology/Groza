d6bdcc6d0c3833ecbedf304feebacf8d811cec3c
sanshengshui <lovewsic@gmail.com>
2018-10-29 19:35:28 +0800

    :beers: 沉迷于代码中

---
+++ transport/mqtt/src/main/java/com/sanshengshui/server/transport/mqtt/MqttTransprotService.java
@@ -0,0 +1,11 @@
+package com.sanshengshui.server.transport.mqtt;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author james
+ * @date 2018年10月29日 19:33
+ */
+@Service("MqttTransportService")
+public class MqttTransprotService {
+}
