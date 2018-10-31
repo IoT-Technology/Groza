9e1c0b99f88cca3203ed4f2fe1c169e542c5a051
sanshengshui <lovewsic@gmail.com>
2018-10-31 16:15:24 +0800

    :building_construction: 初步构建成功

--- application/src/main/java/com/sanshengshui/server/GrozaServerApplication.java
+++ application/src/main/java/com/sanshengshui/server/GrozaServerApplication.java
@@ -1,4 +1,11 @@
 package com.sanshengshui.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.sanshengshui.server"})
 public class GrozaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrozaServerApplication.class);
+    }
 }
