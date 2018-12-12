c625624dcf480b4ee747c11cdf66a680c88fb1fe
sanshengshui <lovewsic@gmail.com>
2018-12-12 17:19:33 +0800

    :heavy_plus_sign: 添加Mybatis依赖包

--- transport/mqtt/src/main/java/com/sanshengshui/server/transport/mqtt/protocol/Connect.java
+++ transport/mqtt/src/main/java/com/sanshengshui/server/transport/mqtt/protocol/Connect.java
@@ -2,8 +2,9 @@ package com.sanshengshui.server.transport.mqtt.protocol;
 
 import io.netty.channel.Channel;
 import io.netty.handler.codec.mqtt.*;
-import io.netty.util.internal.StringUtil;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 /**
  * @author james mu
@@ -38,6 +39,18 @@ public class Connect {
                 return;
             }
         }
+        // clientId为空或null的情况，这里要求客户端必须提供clientId,不管cleanSession是否为1，此处没有参考标准协议实现
+        if(StringUtils.isEmpty(msg.payload().clientIdentifier())){
+            MqttConnAckMessage connAckMessage = (MqttConnAckMessage) MqttMessageFactory.newMessage(
+                    new MqttFixedHeader(MqttMessageType.CONNACK, false, MqttQoS.AT_MOST_ONCE, false, 0),
+                    new MqttConnAckVariableHeader(MqttConnectReturnCode.CONNECTION_REFUSED_IDENTIFIER_REJECTED, false), null);
+            channel.writeAndFlush(connAckMessage);
+            channel.close();
+            return;
+        }
+        // 用户名和密码验证, 这里要求客户端连接时必须提供用户名和密码, 不管是否设置用户名标志和密码标志为1, 此处没有参考标准协议实现
+        String username = msg.payload().userName();
+        String password = msg.payload().passwordInBytes() == null ? null : new String(msg.payload().passwordInBytes(), CharsetUtil.UTF_8);
 
 
     }
