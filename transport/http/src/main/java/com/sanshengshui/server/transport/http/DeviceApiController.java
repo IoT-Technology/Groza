3a0d47262916b408e8b889ac4d18f7e58653facb
sanshengshui <lovewsic@gmail.com>
2019-01-23 17:09:38 +0800

    :construction: Http添加限制类,并未实现逻辑!

--- transport/http/src/main/java/com/sanshengshui/server/transport/http/DeviceApiController.java
+++ transport/http/src/main/java/com/sanshengshui/server/transport/http/DeviceApiController.java
@@ -8,10 +8,14 @@ import com.sanshengshui.server.common.data.kv.AttributeKvEntry;
 import com.sanshengshui.server.common.data.kv.BaseAttributeKvEntry;
 import com.sanshengshui.server.common.data.kv.KvEntry;
 import com.sanshengshui.server.common.data.kv.StringDataEntry;
+import com.sanshengshui.server.common.transport.SessionMsgProcessor;
 import com.sanshengshui.server.common.transport.adaptor.JsonConverter;
+import com.sanshengshui.server.common.transport.auth.DeviceAuthService;
+import com.sanshengshui.server.common.transport.quota.host.HostRequestsQuotaService;
 import com.sanshengshui.server.dao.attributes.AttributesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +34,19 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/api/v1")
 @Slf4j
 public class DeviceApiController {
+
+    @Value("${http.request_timeout}")
+    private long defaultTimeout;
+
+    @Autowired(required = false)
+    private SessionMsgProcessor processor;
+
+    @Autowired(required = false)
+    private DeviceAuthService authService;
+
+    @Autowired(required = false)
+    private HostRequestsQuotaService quotaService;
+
     @Autowired
     private AttributesService attributesService;
 
