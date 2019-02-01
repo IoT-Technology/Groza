3d2b214a14c181586bb0af9d687aae7e113e7d8a
sanshengshui <lovewsic@gmail.com>
2019-02-01 10:03:49 +0800

    :heavy_plus_sign: 添加commons-csv依赖jar包

---
+++ application/src/main/java/com/sanshengshui/server/controller/AssetController.java
@@ -0,0 +1,22 @@
+package com.sanshengshui.server.controller;
+
+import com.sanshengshui.server.common.data.asset.Asset;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author james mu
+ * @date 19-1-31 上午10:35
+ * @description
+ */
+@RestController
+@RequestMapping
+public class AssetController extends BaseController{
+
+    public static final String ASSET_ID = "assetId";
+
+    @RequestMapping(value = "/asset", method = RequestMethod.POST)
+    @ResponseBody
+    public Asset saveAsset(@RequestBody Asset asset){
+        return asset;
+    }
+}
