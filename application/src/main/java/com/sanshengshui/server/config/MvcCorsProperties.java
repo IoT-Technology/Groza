3f0fc17d7805f355f7b80128eef060dd1f60c4af
sanshengshui <lovewsic@gmail.com>
2019-03-18 15:10:24 +0800

    :bento: 跨域配置信息和配置类

---
+++ application/src/main/java/com/sanshengshui/server/config/MvcCorsProperties.java
@@ -0,0 +1,33 @@
+package com.sanshengshui.server.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author james mu
+ * @date 19-3-18 下午3:05
+ * @description
+ */
+@Configuration
+@EnableConfigurationProperties(MvcCorsProperties.class)
+@ConfigurationProperties(prefix = "spring.mvc.cors")
+public class MvcCorsProperties {
+    private Map<String, CorsConfiguration> mappings = new HashMap<>();
+
+    public MvcCorsProperties(){
+        super();
+    }
+
+    public Map<String, CorsConfiguration> getMappings(){
+        return mappings;
+    }
+
+    public void setMappings(Map<String, CorsConfiguration> mappings){
+        this.mappings = mappings;
+    }
+}
